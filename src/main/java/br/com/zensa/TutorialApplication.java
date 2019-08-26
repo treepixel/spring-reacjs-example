4a57fd963f6970d2b94b89c467ac478ceed2048e
Antonio Rodrigues Pinto Junior <antonio_rodrigues@zensa.com.br>
2019-08-26 16:02:06 -0300

    initial commit

---
+++ src/main/java/br/com/zensa/TutorialApplication.java
@@ -0,0 +1,13 @@
+package br.com.zensa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialApplication.class, args);
+	}
+
+}
